34a0bed9a722c96a74e760dbd2a36fdbeb78921d
Matheus <limarios18@outlook.com>
2024-03-01 00:18:36 -0300

    Configurando reducer do quiz

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -9,16 +9,16 @@ import "./Welcome.css"
 
 const Welcome = () => {
 
-  const quizState = useContext(QuizContext);
-
-  console.log(quizState);
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
-        <h2>Seja bem-vindo</h2>
-        <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Inicio do Quiz" />
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Inicio do Quiz" />
     </div>
   )
 }
