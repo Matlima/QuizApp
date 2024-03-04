9269f3b7539ded0485d7231668e11c7f982622d0
Matheus <limarios18@outlook.com>
2024-03-04 19:25:26 -0300

    Concluindo funcionalidade de opções

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -42,6 +42,7 @@ const quizReducer = (state, action) => {
                 ...state,
                 currentQuestion: nexQuestion,
                 gameStage: endGame ? STAGES[2] : state.gameStage,
+                answerSelected: false,
             };
 
         case "NEW_GAME":
@@ -49,7 +50,7 @@ const quizReducer = (state, action) => {
 
         case "CHECK_ANSWER":
             if (state.answerSelected) return state;
-            
+
             const answer = action.payload.answer;
             const option = action.payload.option;
             let correctAnswer = 0;
